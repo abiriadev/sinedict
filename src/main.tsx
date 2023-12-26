6b8fc0a002f33391136b3dcb17ce904326285836
Abiria <abiria.dev@gmail.com>
2023-12-26 16:31:01 +0900

    feat: use custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ConfigProvider } from 'antd'
+import { theme } from './theme.ts'
 
 ReactDOM.createRoot(
 	document.getElementById('root')!,
 ).render(
 	<React.StrictMode>
-		<App />
+		<ConfigProvider theme={theme}>
+			<App />
+		</ConfigProvider>
 	</React.StrictMode>,
 )
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { ThemeConfig } from 'antd'
+
+export const theme: ThemeConfig = {
+	token: {
+		colorPrimary: '#9BD3F8',
+	},
+}
