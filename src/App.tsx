acfa89e72e3aaf9a6e68af9a9ad14335501961ea
Abiria <abiria.dev@gmail.com>
2023-12-26 15:12:54 +0900

    feat: fix header typography style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
 import { useState } from 'react'
-import { ConfigProvider, Flex, Layout, Space } from 'antd'
+import {
+	ConfigProvider,
+	Flex,
+	Layout,
+	Space,
+	Typography,
+} from 'antd'
 import { ArticleData } from './interface'
 import { Article } from './Article'
 import { Content, Header } from 'antd/es/layout/layout'
 
 const data: Array<ArticleData> = [
+	{
+		word: 'asdf',
+		description: 'adfada',
+		example: 'adfa',
+		up: 10,
+		down: 2,
+	},
+	{
+		word: 'siba',
+		description: 'sine bye',
+		example: 'siba~',
+		up: 5,
+		down: 3,
+	},
+	{
+		word: 'asdf',
+		description: 'adfada',
+		example: 'adfa',
+		up: 10,
+		down: 2,
+	},
+	{
+		word: 'siba',
+		description: 'sine bye',
+		example: 'siba~',
+		up: 5,
+		down: 3,
+	},
+	{
+		word: 'asdf',
+		description: 'adfada',
+		example: 'adfa',
+		up: 10,
+		down: 2,
+	},
+	{
+		word: 'siba',
+		description: 'sine bye',
+		example: 'siba~',
+		up: 5,
+		down: 3,
+	},
 	{
 		word: 'asdf',
 		description: 'adfada',
@@ -27,17 +75,28 @@ function App() {
 	return (
 		<ConfigProvider
 			theme={{
+				token: {
+					fontSizeHeading1: 30,
+				},
 				components: {
 					Layout: {
 						headerBg: 'inherit',
 						bodyBg: 'inherit',
+					},
+					Typography: {
+						titleMarginBottom: 0,
+						titleMarginTop: 0,
 					},
 				},
 			}}
 		>
 			<Layout>
 				<Header className="sticky top-0">
-					UpDict
+					<Flex align="center" className="h-full">
+						<Typography.Title level={1}>
+							UpDict
+						</Typography.Title>
+					</Flex>
 				</Header>
 				<Content>
 					<Flex justify="center">
