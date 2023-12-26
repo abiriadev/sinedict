d380fec4636dd5d19704e62f7db283ec43f5f56d
Abiria <abiria.dev@gmail.com>
2023-12-26 18:17:06 +0900

    refactor: extract appbar into a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import { useState } from 'react'
-import {
-	Avatar,
-	Button,
-	ConfigProvider,
-	Dropdown,
-	Flex,
-	Layout,
-	Typography,
-	theme,
-} from 'antd'
+import { Button, ConfigProvider, Flex, Layout } from 'antd'
 import { ArticleData } from './interface'
 import { Article } from './Article'
-import {
-	Content,
-	Footer,
-	Header,
-} from 'antd/es/layout/layout'
-import {
-	LogoutOutlined,
-	PlusOutlined,
-	UserOutlined,
-} from '@ant-design/icons'
-import { FaDiscord, FaGithub } from 'react-icons/fa'
-import { ImTwitch } from 'react-icons/im'
+import { Content, Footer } from 'antd/es/layout/layout'
+import { PlusOutlined } from '@ant-design/icons'
 import { NewArticle } from './NewArticle'
-import { IconContext } from 'react-icons'
+import { AppBar } from './AppBar'
 
 const data: Array<ArticleData> = [
 	{
@@ -88,7 +69,6 @@ const data: Array<ArticleData> = [
 function App() {
 	const [articles, _] = useState(data)
 	const [isModalOpen, setIsModalOpen] = useState(false)
-	const { token } = theme.useToken()
 
 	return (
 		<ConfigProvider
@@ -103,68 +83,7 @@ function App() {
 			}}
 		>
 			<Layout>
-				<Header className="sticky top-0 z-10 backdrop-blur-md">
-					<Flex
-						align="center"
-						justify="space-between"
-						className="h-full"
-					>
-						<Typography.Title level={1}>
-							UpDict
-						</Typography.Title>
-						<IconContext.Provider
-							value={{
-								color: token.colorPrimary,
-								className: 'text-3xl',
-							}}
-						>
-							<Flex
-								gap="large"
-								align="center"
-							>
-								<a
-									href=""
-									className="leading-none"
-								>
-									<ImTwitch />
-								</a>
-								<a
-									href=""
-									className="leading-none"
-								>
-									<FaDiscord />
-								</a>
-								<a
-									href=""
-									className="leading-none"
-								>
-									<FaGithub />
-								</a>
-								<Dropdown
-									menu={{
-										items: [
-											{
-												key: '1',
-												label: 'Sign out',
-												danger: true,
-												icon: (
-													<LogoutOutlined />
-												),
-											},
-										],
-									}}
-								>
-									<Avatar
-										className="text-3xl"
-										icon={
-											<UserOutlined />
-										}
-									/>
-								</Dropdown>
-							</Flex>
-						</IconContext.Provider>
-					</Flex>
-				</Header>
+				<AppBar />
 				<Content>
 					<Flex justify="center">
 						<Flex
---
+++ src/AppBar.tsx
@@ -0,0 +1,70 @@
+import {
+	LogoutOutlined,
+	UserOutlined,
+} from '@ant-design/icons'
+import {
+	Avatar,
+	Dropdown,
+	Flex,
+	Typography,
+	theme,
+} from 'antd'
+import { Header } from 'antd/es/layout/layout'
+import { IconContext } from 'react-icons'
+import { FaDiscord, FaGithub } from 'react-icons/fa'
+import { ImTwitch } from 'react-icons/im'
+
+export const AppBar = () => {
+	const { token } = theme.useToken()
+
+	return (
+		<Header className="sticky top-0 z-10 backdrop-blur-md">
+			<Flex
+				align="center"
+				justify="space-between"
+				className="h-full"
+			>
+				<Typography.Title level={1}>
+					UpDict
+				</Typography.Title>
+				<IconContext.Provider
+					value={{
+						color: token.colorPrimary,
+						className: 'text-3xl',
+					}}
+				>
+					<Flex gap="large" align="center">
+						<a href="" className="leading-none">
+							<ImTwitch />
+						</a>
+						<a href="" className="leading-none">
+							<FaDiscord />
+						</a>
+						<a href="" className="leading-none">
+							<FaGithub />
+						</a>
+						<Dropdown
+							menu={{
+								items: [
+									{
+										key: '1',
+										label: 'Sign out',
+										danger: true,
+										icon: (
+											<LogoutOutlined />
+										),
+									},
+								],
+							}}
+						>
+							<Avatar
+								className="text-3xl"
+								icon={<UserOutlined />}
+							/>
+						</Dropdown>
+					</Flex>
+				</IconContext.Provider>
+			</Flex>
+		</Header>
+	)
+}
