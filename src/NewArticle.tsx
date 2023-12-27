12c0477c70bcee60215a5458604478bb8dacad74
Abiria <abiria.dev@gmail.com>
2023-12-27 14:35:11 +0900

    feat: implement non-interactive form

--- src/NewArticle.tsx
+++ src/NewArticle.tsx
@@ -1,4 +1,4 @@
-import { Modal } from 'antd'
+import { Form, Input, Modal } from 'antd'
 
 export interface NewArticleProps {
 	open: boolean
@@ -6,12 +6,53 @@ export interface NewArticleProps {
 	onCancel: () => void
 }
 
+export interface FieldType {
+	word: string
+	description: string
+	example: string
+}
+
 export const NewArticle = (props: NewArticleProps) => {
 	return (
 		<Modal title="New article" {...props}>
-			<p>Word</p>
-			<p>Description</p>
-			<p>Example</p>
+			<Form layout="vertical">
+				<Form.Item<FieldType>
+					label="Word"
+					rules={[
+						{
+							required: true,
+							message:
+								'Please fill this form',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item<FieldType>
+					label="Description"
+					rules={[
+						{
+							required: true,
+							message:
+								'Please fill this form',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item<FieldType>
+					label="Example"
+					rules={[
+						{
+							required: true,
+							message:
+								'Please fill this form',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+			</Form>
 		</Modal>
 	)
 }
