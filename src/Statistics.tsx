88a2073cba728321f79aaaed5a4f934bb7aa5a53
Abiria <abiria.dev@gmail.com>
2023-12-28 18:34:04 +0900

    feat: show author information

--- src/Statistics.tsx
+++ src/Statistics.tsx
@@ -1,4 +1,5 @@
-import { Modal } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import { Avatar, Modal, Typography } from 'antd'
 
 export interface StatisticsProp {
 	open: boolean
@@ -15,6 +16,12 @@ export const Statistics = ({
 			open={open}
 			onCancel={() => setOpen(false)}
 			footer={() => <></>}
-		></Modal>
+		>
+			<Typography.Title level={3}>
+				Author
+			</Typography.Title>
+			<Avatar icon={<UserOutlined />} />
+			<Typography.Text>Username</Typography.Text>
+		</Modal>
 	)
 }
