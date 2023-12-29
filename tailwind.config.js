c2be1ba10a125ecb719d6614bea41668d3ea4bc2
Abiria <abiria.dev@gmail.com>
2023-12-29 15:58:13 +0900

    feat: use extend instead of overriding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
-		colors: {
-			primary: {
-				DEFAULT: '#5CA0CE',
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#5CA0CE',
+				},
 			},
 		},
 	},
