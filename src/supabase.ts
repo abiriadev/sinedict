5a574eb96ed7ff8247b399de832dcdf5f8e9f99d
Abiria <abiria.dev@gmail.com>
2023-12-29 20:04:15 +0900

    build: move supabase settings to .env file

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
 export const supabase = createClient<Database>(
-	'https://citoovmxdeourtuxvqxa.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpdG9vdm14ZGVvdXJ0dXh2cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2NDMwNTksImV4cCI6MjAxOTIxOTA1OX0.N7ekN5G3Gpy4oGSpIh-4tiSktVL2WDBQ_IH8MtzOtAU',
+	import.meta.env.VITE_SUPABASE_ENDPOINT,
+	import.meta.env.VITE_SUPABASE_ANON,
 )
