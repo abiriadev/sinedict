a1cb7201e959cae6b46f181d6de868b3720143fe
Abiria <abiria.dev@gmail.com>
2023-12-29 19:38:57 +0900

    build: set up GA radar

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '')
 
 	return {
-		plugins: [react()],
+		plugins: [
+			react(),
+			VitePluginRadar({
+				analytics: {
+					id: env.GA_MEASUREMENT_ID,
+				},
+			}),
+		],
 	}
 })
